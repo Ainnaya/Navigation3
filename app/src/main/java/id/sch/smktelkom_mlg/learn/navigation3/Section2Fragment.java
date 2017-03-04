eae8e938b97ae285dd9c1d818a66f41c07459bcd
Ainnaya <Ainnaya56@gmail.com>
2017-03-04 20:24:21 +0700

    Action Bar Spinner Change Fragment

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/navigation3/Section2Fragment.java
@@ -0,0 +1,23 @@
+package id.sch.smktelkom_mlg.learn.navigation3;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Section2Fragment extends Fragment {
+
+
+    public Section2Fragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_section2, container, false);
+    }
+
+}
